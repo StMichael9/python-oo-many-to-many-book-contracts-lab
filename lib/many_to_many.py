f3a94812a8ac3e8e4c9a812bcd831ab688e1e596
St Michael Egenamba <michaelegenamba@gmail.com>
2026-06-04 16:39:24 -0700

    Finished author class moving on to book

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,50 @@
 class Author:
-    pass
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+
+        Author.all.append(self)
+    
+    def contracts(self):
+        author_contracts = []
+        for contract in Contract.all:
+            if contract.author == self:
+                author_contracts.append(contract)
+        return author_contracts
+    
+    def books(self):
+        author_books = []
+        for contract in self.contracts():
+            author_books.append(contract.book)
+        return author_books
+    
+    def sign_contracts(self, book, date, royalties):
+        new_contract = Contract(self, book, date, royalties)
+        return new_contract
+
+    def total_royalties(self):
+        pass
+    
+       
 
 
 class Book:
-    pass
+    all = []
+    def __init__(self, title):
+        self.title = title
 
+        Book.all.append(self)
+    
+        
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date =  date
+        self.royalties = royalties
+
+        Contract.all.append(self)
+        
